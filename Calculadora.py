abfcfa34cf1da6db30bbbcf33316bf8af1a28217
Line-chereca <166437506+Line-chereca@users.noreply.github.com>
2024-10-29 09:11:38 +0000

    Update and rename Calculadora_Simples.py to Calculadora.py
    adicionei a função de multiplicação

--- Calculadora.py
+++ Calculadora.py
@@ -5,6 +5,11 @@ def soma(x, y):
 # Função para subtração
 def subtração(x, y):
     return x - y
+
+# Função para multiplicação
+def multiplicação(x, y):
+    return x * y
+    
 #Função principal
 def calculadora() :
         operador = input("Digite a operação: ")
@@ -12,12 +17,16 @@ def calculadora() :
         num2 = int(input("Digite o segundo número: "))
         result1 = soma(num1, num2)
         result2 = subtração(num1, num2)
+        result3 = multiplicação(num1, num2)
 
         if operador == "soma":
             print("Resultado da soma:" + str(result1))
 
         elif operador == "sub":
             print("Resultado da subtração:" + str(result2))
+
+         elif operador == "multi":
+            print("Resultado da multiplicação:" + str(result3))
 
 # Executa a calculadora
 calculadora()
