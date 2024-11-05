54f4fd93dc4f4bfc4689ea0a646971653f6f36dc
Line-chereca <166437506+Line-chereca@users.noreply.github.com>
2024-11-05 09:47:36 +0000

    validação dos números

--- Calculadora.py
+++ Calculadora.py
@@ -13,3 +13,28 @@ def multiplicação(x, y):
 # Função para divisão
 def divisão(x, y):
     return x / y
+
+#Função principal
+def calculadora() :
+    lista_contas = []
+    while True:
+        operador = input("Digite a operação: ")
+        if operador != "Hist":
+         while True:
+            try:
+                num1 = int(input("Digite o primeiro número: "))
+            except ValueError:
+                print("Seu burro")
+                continue
+            else:
+                print("Bem da caro")
+                break
+         while True:
+            try:
+                num2 = int(input("Digite o segundo número: "))
+            except ValueError:
+                print("Seu burro")
+                continue
+            else:
+                print("Bem da caro")
+                break
