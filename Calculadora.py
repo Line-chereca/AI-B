c755798ea50316ac1d7b48160f6ac0eb153c59b6
Line-chereca <166437506+Line-chereca@users.noreply.github.com>
2024-11-05 09:46:36 +0000

    adicionei funções

---
+++ Calculadora.py
@@ -0,0 +1,15 @@
+# Função para soma
+def soma(x, y):
+    return x + y
+
+# Função para subtração
+def subtração(x, y):
+    return x - y
+
+# Função para multiplicação
+def multiplicação(x, y):
+    return x * y
+
+# Função para divisão
+def divisão(x, y):
+    return x / y
